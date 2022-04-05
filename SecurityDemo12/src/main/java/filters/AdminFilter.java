5ca7f16be26a463bcb943d1c7c77149ecdaee6b3
bebehgurl <97761692+bebehgurl@users.noreply.github.com>
2022-04-04 20:02:41 -0600

    Add files via upload

---
+++ SecurityDemo12/src/main/java/filters/AdminFilter.java
@@ -0,0 +1,63 @@
+
+package filters;
+
+import ca.sait.securitydemo12.models.User;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author Owner
+ */
+public class AdminFilter implements Filter {
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession();
+        User user = (User) session.getAttribute("user");
+        
+        //Check if user is an admin or not
+            // if admin:
+            if(user.getRole().getRoleId() == 1) {
+                chain.doFilter(request, response);
+            }
+            //if not admin:
+            else {
+                httpResponse.sendRedirect("/notes");
+            }
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {
+
+    }
+}
